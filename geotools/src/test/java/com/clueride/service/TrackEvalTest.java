8385f2f7fbc1af1d408386e05bc42e75534648ec
jett <jettmarks@gmail.com>
2015-12-24 10:22:51 -0500

    CA-56 Segment Recommendations sometimes fall short of connecting to the network

--- geotools/src/test/java/com/clueride/service/TrackEvalTest.java
+++ geotools/src/test/java/com/clueride/service/TrackEvalTest.java
@@ -1,4 +1,25 @@
+/*
+ *   Copyright 2015 Jett Marks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created Nov 03, 2015
+ */
 package com.clueride.service;
+
+import com.vividsolutions.jts.geom.Point;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import com.clueride.config.GeoProperties;
 import com.clueride.dao.DefaultTrackStore;
@@ -8,17 +29,15 @@ import com.clueride.domain.factory.PointFactory;
 import com.clueride.feature.Edge;
 import com.clueride.feature.TrackFeature;
 import com.clueride.geo.SplitLineString;
-import com.vividsolutions.jts.geom.Point;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 import static com.clueride.geo.SplitLineString.END;
+import static com.clueride.geo.SplitLineString.START;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertNotNull;
 import static org.testng.AssertJUnit.assertTrue;
 
 /**
- * Created by jett on 11/3/15.
+ * Exercises the TrackEval class, particularly the determination of recommendable
+ * tracks and the "splitting node".
  */
 public class TrackEvalTest {
 
@@ -53,12 +72,12 @@ public class TrackEvalTest {
 
     @Test
     public void testGetNodeDistance() throws Exception {
-        System.out.println("Node Distance: "+toTest.getNodeDistance());
+        System.out.println("Node Distance: " + toTest.getNodeDistance());
     }
 
     @Test
     public void testGetEdgeDistance() throws Exception {
-        System.out.println("Edge Distance: "+toTest.getEdgeDistance());
+        System.out.println("Edge Distance: " + toTest.getEdgeDistance());
     }
 
     @Test
@@ -88,6 +107,30 @@ public class TrackEvalTest {
 
 //        TrackFeature proposedTrack = toTest.getProposedTrack();
 //        assertEquals(splittingNode.getPoint(), proposedTrack.getLineString().getEndPoint());
+
+        boolean coverage = nearestEdge.getLineString().buffer(GeoProperties.BUFFER_TOLERANCE).covers(splittingNode.getPoint());
+        assertTrue(coverage);
+    }
+
+    @Test
+    public void testGetSplittingNodePiedmontPark() throws Exception {
+        Point point = PointFactory.getJtsInstance(33.78639, -84.377575, 270.4);
+        GeoNode newNode = new DefaultGeoNode(point);
+        TrackFeature track = DefaultTrackStore.getInstance().getTrackById(20);
+        assertNotNull(track);
+        SplitLineString splitLineString = new SplitLineString(track, newNode);
+        toTest = new TrackEval(splitLineString.getSubTrackFeature(START));
+
+        Edge nearestEdge = toTest.getNearestNetworkEdge();
+        assertNotNull(nearestEdge);
+        System.out.println("Nearest Edge: " + nearestEdge);
+
+        GeoNode splittingNode = toTest.getSplittingNode();
+        assertNotNull(splittingNode);
+        System.out.println("Splitting Node as Point: "+splittingNode.getPoint());
+
+        TrackFeature proposedTrack = toTest.getProposedTrack();
+        assertEquals(splittingNode.getPoint(), proposedTrack.getLineString().getEndPoint());
 
         boolean coverage = nearestEdge.getLineString().buffer(GeoProperties.BUFFER_TOLERANCE).covers(splittingNode.getPoint());
         assertTrue(coverage);
