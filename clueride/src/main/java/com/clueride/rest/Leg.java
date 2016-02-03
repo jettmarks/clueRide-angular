e0e06d3038ef1aba10cde8cfb9cab05093f55c62
jett <jettmarks@gmail.com>
2016-02-02 22:34:57 -0500

    CA-107 Service for retrieving Paths
    Speedy version suitable for getting the features onto a leaflet map.

---
+++ clueride/src/main/java/com/clueride/rest/Leg.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2016 Jett Marks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Created by jett on 2/2/16.
+ */
+package com.clueride.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.clueride.service.PathService;
+
+/**
+ * REST API for working with a Leg.
+ *
+ * This would have been called Path, but that name collides with the Jersey Annotation.
+ *
+ * This "reveals" the course one chunk at a time, the chunks that lead from one location
+ * to another.
+ */
+@Path("path")
+public class Leg {
+    private PathService pathService;
+
+    @Inject
+    public Leg(PathService pathService) {
+        this.pathService = pathService;
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("map")
+    public String getPathGeometry(@QueryParam("pathId") Integer pathId) {
+        return pathService.getPathGeometry(pathId);
+    }
+}
--- crCore/src/main/java/com/clueride/service/builder/PathBuilder.java
+++ crCore/src/main/java/com/clueride/service/builder/PathBuilder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- * Created by jett on 1/17/16.
+ * Created by jett on 2/2/16.
  */
 package com.clueride.service.builder;
 
@@ -24,8 +24,6 @@ import org.opengis.feature.simple.SimpleFeature;
 
 import com.clueride.dao.NetworkStore;
 import com.clueride.dao.NodeStore;
-import com.clueride.dao.PathStore;
-import com.clueride.domain.Course;
 import com.clueride.domain.GeoNode;
 import com.clueride.domain.user.Path;
 import com.clueride.feature.Edge;
@@ -34,51 +32,44 @@ import com.clueride.io.GeoJsonUtil;
 import com.clueride.io.JsonStoreType;
 
 /**
- * Assembles a Feature Collection for a Course so it can be displayed on a map.
+ * Assembles a Feature Collection for a Path so it can be displayed on a map.
  */
-public class CourseBuilder {
-    private final PathStore pathStore;
+public class PathBuilder {
     private final NetworkStore networkStore;
     private final NodeStore nodeStore;
 
     @Inject
-    public CourseBuilder(
-            PathStore pathStore,
+    public PathBuilder(
             NetworkStore networkStore,
             NodeStore nodeStore
     ) {
-        this.pathStore = pathStore;
         this.networkStore = networkStore;
         this.nodeStore = nodeStore;
     }
 
-    public String getCourseFeatureCollection(Course course) {
+
+    public String getPathFeatureCollection(Path path) {
         GeoJsonUtil jsonRespWriter = new GeoJsonUtil(JsonStoreType.OTHER);
         DefaultFeatureCollection fcPoints = new DefaultFeatureCollection();
         DefaultFeatureCollection fcNonPoints = new DefaultFeatureCollection();
-        Path path = null;
         GeoNode node;
-        for (Integer pathId : course.getPathIds()) {
-            // TODO: Refactor this to use what is already done in the Path Builder.
-            path = pathStore.getPathById(pathId);
-            node = (GeoNode) nodeStore.getNodeById(path.getStartNodeId());
-            fcPoints.add(TranslateUtil.geoNodeToFeature(node));
-            for (Integer edgeId : path.getEdgeIds()) {
-                Edge edge = networkStore.getEdgeById(edgeId);
-                fcNonPoints.add(edge.getFeature());
-            }
+        node = (GeoNode) nodeStore.getNodeById(path.getStartNodeId());
+        fcPoints.add(TranslateUtil.geoNodeToFeature(node));
+        for (Integer edgeId : path.getEdgeIds()) {
+            Edge edge = networkStore.getEdgeById(edgeId);
+            fcNonPoints.add(edge.getFeature());
         }
-        if (path == null) {
-            throw new IllegalStateException("No paths in the course");
-        }
+
         node = (GeoNode) nodeStore.getNodeById(path.getEndNodeId());
         fcPoints.add(TranslateUtil.geoNodeToFeature(node));
+
         String pointResult = jsonRespWriter.toString(fcPoints);
         StringBuilder stringBuffer = new StringBuilder();
         stringBuffer.append(pointResult.substring(0, pointResult.length()-2));
         for (SimpleFeature feature : fcNonPoints) {
             stringBuffer.append(",").append(jsonRespWriter.toString(feature));
         }
+
         stringBuffer.append("]}");
         return stringBuffer.toString();
     }
